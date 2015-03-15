be9071d77cf48cf0a505cc89ed669a4f3f4ee4b4
ke0m <joseph.s.jennings@gmail.com>
2015-03-15 00:44:35 -0600

    Incorporated tolerance

--- w1-l3/mysqrt.py
+++ w1-l3/mysqrt.py
@@ -1,13 +1,16 @@
 import numpy as np
 
-s = 500
-x = 1000000
-kmax = 6
+s = 1
+x = 2
+kmax = 100
+tol = 1.e-14
 
 for k in range(kmax):
     print "Before iteration %s, s= %s" %(k,s)
-    s = 0.5 * (s + x/s)
+    s0 = s
+    s = 0.5 * (s + x/s) #Newton's method
+    delta_s = s-s0
+    if abs(delta_s/x) < tol: #checking to see that the relative error lies within 
+        break                #a specified tolerance
 
-print "After %s iterations, s = %s" %(kmax,s)
-
-
+print "After %s iterations, s = %s" %(k+1,s)
