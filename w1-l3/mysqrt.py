997bea14b70cd740be0f9092b78a05c2bfbc1001
ke0m <joseph.s.jennings@gmail.com>
2015-03-15 00:58:08 -0600

    Now is a function

--- w1-l3/mysqrt.py
+++ w1-l3/mysqrt.py
@@ -1,16 +1,15 @@
 import numpy as np
 
-s = 1
-x = 2
-kmax = 100
-tol = 1.e-14
-
-for k in range(kmax):
-    print "Before iteration %s, s= %s" %(k,s)
-    s0 = s
-    s = 0.5 * (s + x/s) #Newton's method
-    delta_s = s-s0
-    if abs(delta_s/x) < tol: #checking to see that the relative error lies within 
-        break                #a specified tolerance
-
-print "After %s iterations, s = %s" %(k+1,s)
+def sqrt2(x):
+    s = 1
+    kmax = 100
+    tol = 1.e-14
+    for k in range(kmax):
+        print "Before iteration %s, s= %s" % (k,s)
+        s0 = s
+        s = 0.5 * (s + x/s) #Newton's method
+        delta_s = s-s0
+        if abs(delta_s/x) < tol: #checking to see that the relative error lies within 
+            break                #a specified tolerance
+    
+    print "After %s iterations, s = %s" %(k+1,s)
