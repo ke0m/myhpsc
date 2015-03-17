3f6e4431faaaf0e073564f06c2fd451e531cc7c4
ke0m <joseph.s.jennings@gmail.com>
2015-03-16 21:18:20 -0600

    The second part of HW2

---
+++ hw2/hw2b.py
@@ -0,0 +1,52 @@
+import numpy as np
+from mpl_toolkits.axes_grid.axislines import SubplotZero
+from matplotlib import pyplot as plt
+from numpy import linalg as la
+
+def quad_interp(xi, yi, debug=False):
+
+    #dimensions of matrix
+    n = len(xi)
+    m = 3
+
+    #plotting the input points
+    fig = plt.figure(1)
+    ax = SubplotZero(fig, 111)
+    fig.add_subplot(ax)
+
+    ax.plot(xi, yi, 'ro')
+    ax.axis([-10,10,-10,10])
+
+    for direction in ["xzero", "yzero"]:
+        ax.axis[direction].set_axisline_style("-|>")
+        ax.axis[direction].set_visible(True)
+
+    for direction in ["left", "right", "bottom", "top"]:
+        ax.axis[direction].set_visible(False)
+
+    #Constructing A
+
+    A = np.zeros([n,m]) #takes a list as input
+    for i in range(n):
+        A[i][0] = 1
+        A[i][1] = xi[i]
+        A[i][2] = xi[i]**2
+
+    c = la.solve(A,yi)
+
+    if debug==True:
+        print A
+        print c
+
+    #plotting the interpolated points
+    xo = np.linspace(-10, 10, 21)
+    yo = np.zeros(21)
+    for i in range(21):
+        yo[i] = c[0] + c[1]*(i-10) + c[2]*(i-10)**2
+
+    ax.plot(xo, yo, 'b')
+
+    plt.savefig('hw2b.png')
+    plt.show()
+
+    return c
