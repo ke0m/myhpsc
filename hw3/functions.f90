24a3d828905584a0bf35a06cd3427f811b493c85
ke0m <joseph.s.jennings@gmail.com>
2015-04-02 09:17:04 -0600

    The module containing the functions for hw3

---
+++ hw3/functions.f90
@@ -0,0 +1,77 @@
+! $MYHPSC/w4-l10/newton/functions.f90
+
+module functions
+
+  !real(kind=8), parameter :: pi = acos(-1)
+  real(kind=8), parameter :: pi = 3.14159265358979323
+
+contains
+
+real(kind=8) function f_sqrt(x)
+  implicit none
+  real(kind=8), intent(in) :: x
+
+  f_sqrt = x**2 - 4.d0 ! Hardcoding four means we want to find the square root of 4
+
+end function f_sqrt
+
+
+real(kind=8) function fprime_sqrt(x)
+  implicit none
+  real(kind=8), intent(in) :: x
+
+  fprime_sqrt = 2.d0*x
+
+end function fprime_sqrt
+
+
+real(kind=8) function g1(x)
+  implicit none
+  real(kind=8), intent(in) :: x
+
+  g1 = x*cos(pi*x)
+
+end function g1
+
+real(kind=8) function g1_prime(x)
+  implicit none
+  real(kind=8), intent(in) :: x
+
+  g1_prime = cos(pi*x) - x*pi*sin(pi*x)
+
+end function g1_prime
+
+real(kind=8) function g2(x)
+  implicit none
+  real(kind=8), intent(in) :: x
+
+  g2 = 1 - 0.6*x**2
+
+end function g2
+
+real(kind=8) function g2_prime(x)
+  implicit none
+  real(kind=8), intent(in) :: x
+
+  g2_prime = -1.2*x**2
+
+end function g2_prime
+
+real(kind=8) function g1_g2(x)
+  implicit none
+  real(kind=8), intent(in) :: x
+
+  g1_g2 = x*cos(pi*x) + 0.6*x**2 - 1
+
+end function g1_g2
+
+real(kind=8) function g1_g2_prime(x)
+  implicit none
+  real(kind=8), intent(in) :: x
+
+  g1_g2_prime = cos(pi*x) - x*pi*sin(pi*x) + 1.2*x
+
+end function
+
+end module functions
+
