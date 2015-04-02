461d945d2a2c1960f989952180a98a7a91bca997
ke0m <joseph.s.jennings@gmail.com>
2015-04-01 20:39:59 -0600

    First attempt at problem 5

---
+++ hw3/intersections.f90
@@ -0,0 +1,27 @@
+! $MYHPSC/hw3/intersections.f90
+
+program intersections
+
+  use newton, only: solve
+  use functions, only: g1, g2, g1_g2, g1_g2_prime
+
+  implicit none
+  real(kind=8) :: x0, fx
+  real(kind=8) :: x(4), x0vals(4)
+  integer :: iters, i
+  logical :: debug = .false.
+
+  ! the four guesses for each intersection
+  x0vals = (/-3.d0, -2.d0, -1.d0, 1.9d0/)
+
+  do i = 1, 4
+    x0 = x0vals(i)
+    print *, ' '
+    call solve(g1_g2, g1_g2_prime, x0, x(i), iters, debug)
+    print 11, x0, x(i), iters
+    11 format('With initial guess x0 = ' e20.15, ' solve returns x = ' e20.15, ' after ' i3 ' iterations.')
+  enddo
+
+end program intersections
+
+
