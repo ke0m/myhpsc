edb1b0c4db9f884224f7c8135597d660dae9039d
ke0m <joseph.s.jennings@gmail.com>
2015-04-14 13:27:37 -0600

    contains the routines for questions 6-8

---
+++ homework/hw5/quadrature3.f90
@@ -0,0 +1,111 @@
+! $MYHPSC/homeworks/quadrature3.f90
+
+module quadrature3
+
+    use omp_lib
+
+contains
+
+real(kind=8) function trapezoid(f, a, b, n)
+
+    ! Estimate the integral of f(x) from a to b using the
+    ! Trapezoid Rule with n points.
+
+    ! Input:
+    !   f:  the function to integrate
+    !   a:  left endpoint
+    !   b:  right endpoint
+    !   n:  number of points to use
+    ! Returns:
+    !   the estimate of the integral
+     
+    implicit none
+    real(kind=8), intent(in) :: a,b
+    real(kind=8), external :: f
+    integer, intent(in) :: n
+
+    ! Local variables:
+    integer :: j
+    real(kind=8) :: h, trap_sum, xj
+
+    h = (b-a)/(n-1)
+    trap_sum = 0.5d0*(f(a) + f(b))  ! endpoint contributions
+    
+    do j=2,n-1
+        xj = a + (j-1)*h
+        trap_sum = trap_sum + f(xj)
+        enddo
+
+    trapezoid = h * trap_sum
+
+end function trapezoid
+
+
+
+real(kind=8) function simpson(f, a, b, n)
+
+  implicit none
+  real(kind=8), intent(in) :: a,b
+  real(kind=8), external :: f
+  integer, intent(in) :: n
+
+  ! Local variables
+  real(kind=8) :: xj, xjo2, simp_sum, h
+  integer :: j
+
+  ! compute the endpoints in the beginning
+  ! and the accumulate the sum with two points
+  ! in each iteration
+
+  h = (b-a)/(n-1)
+  simp_sum =  f(a)
+  !print *,"h= ", h, "simp_sum= ", simp_sum
+
+  !$omp parallel do private(xj,xjo2) reduction(+ : simp_sum)
+  do j=1,n-1
+    xj = a + j*h
+    xjo2 = xj-h/2
+    simp_sum = simp_sum + 4*f(xjo2) + 2*f(xj)
+    !print *,"j= ", j,"xj= ", xj, "xj/2= ", (xjo2), "Simp_sum = ", simp_sum
+  enddo
+
+  simpson = (h/6)*(simp_sum-f(b))
+
+end function simpson
+
+subroutine error_table(f,a,b,nvals,int_true,method)
+
+    ! Compute and print out a table of errors when the quadrature
+    ! rule specified by the input function method is applied for
+    ! each value of n in the array nvals.
+
+    implicit none
+    real(kind=8), intent(in) :: a,b, int_true
+    real(kind=8), external :: f, method
+    integer, dimension(:), intent(in) :: nvals
+    integer :: thread_num = 0
+
+    ! Local variables:
+    integer :: j, n
+    real(kind=8) :: ratio, last_error, error, int_approx
+
+    print *, "      n         approximation        error       ratio    thread num"
+    last_error = 0.d0 
+    !$omp parallel do firstprivate(last_error) private(n,error) &
+    !$omp schedule(dynamic)
+    do j=size(nvals),1,-1 
+        n = nvals(j)
+        int_approx = method(f,a,b,n)
+        error = abs(int_approx - int_true)
+        ratio = last_error / error
+        last_error = error  ! for next n
+        !$ thread_num = omp_get_thread_num()
+
+        print 11, n, int_approx, error, ratio, thread_num
+ 11     format(i8, es22.14, es13.3, es13.3, i8)
+        enddo
+
+end subroutine error_table
+
+
+end module quadrature3
